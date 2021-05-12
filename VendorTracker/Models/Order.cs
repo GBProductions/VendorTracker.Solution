1885bcb628903167c1c017b25236c5380f0c326d
Garrett Brown <garrettpaulbrown@gmail.com>
2021-05-12 15:22:34 -0700

    Create Order.cs

---
+++ VendorTracker/Models/Order.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace VendorTracker.Models
+{
+  public class Order
+  {
+    private static List<Order> _instances = new List<Order> {};
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Vendor> Vendors { get; set; }
+  }
+}
