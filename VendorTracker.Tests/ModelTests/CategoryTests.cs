57cccd50b23ffe54b2c0ea0c9762fba2d96e0d16
Garrett Brown <garrettpaulbrown@gmail.com>
2021-05-12 15:52:36 -0700

    GetId test good fail.

--- VendorTracker.Tests/ModelTests/CategoryTests.cs
+++ VendorTracker.Tests/ModelTests/CategoryTests.cs
@@ -14,5 +14,34 @@ namespace VendorTracker.Tests
             Order newOrder = new Order("test category");
             Assert.AreEqual(typeof(Order), newOrder.GetType());
         }
+
+        [TestMethod]
+        public void GetName_ReturnsName_String()
+        {
+            //Arrange
+            string name = "Test Order";
+            Order newOrder = new Order(name);
+
+            //Act
+            string result = newOrder.Name;
+
+            //Assert
+            Assert.AreEqual(name, result);
+        }
+
+        [TestMethod]
+        public void GetId_ReturnsOrderId_Int()
+        {
+            //Arrange
+            string name = "Test Order";
+            Order newOrder = new Order(name);
+
+            //Act
+            int result = newOrder.Id;
+
+            //Assert
+            Assert.AreEqual(1, result);
+        }
+
     }
 }
