a5870ae8e82a9cd5282f49678ab8aed62071e942
Garrett Brown <garrettpaulbrown@gmail.com>
2021-05-12 11:03:36 -0700

    Vendor Constructor Test.

--- VendorTracker.Tests/ModelTests/Vendor.Tests.cs
+++ VendorTracker.Tests/ModelTests/Vendor.Tests.cs
@@ -1,9 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using ToDoList.Models;
+using VendorTracker.Models;
 using System;
 
 namespace VendorTracker.Tests
 {
-    
+    [TestClass]
+    public class VendorTests : IDisposable 
+    {
+        public void Dispose()
+        {
+            Vendor.ClearAll();
+        }
+
+        [TestMethod]
+        public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+        {
+            Vendor newVendor = new Vendor("test", "test");
+            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+    }
 }
