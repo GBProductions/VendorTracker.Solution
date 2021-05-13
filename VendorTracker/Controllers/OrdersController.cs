e43915360ae4ff451351469e39ea5039aa460d1c
Garrett Brown <garrettpaulbrown@gmail.com>
2021-05-12 17:11:01 -0700

    Add OrdersController.cs.

---
+++ VendorTracker/Controllers/OrdersController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorTracker.Models;
+
+namespace VendorTracker.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/orders")]
+    public ActionResult Index()
+    {
+      List<Order> allOrders = Order.GetAll();
+      return View(allOrders);
+    }
+  }
+}
